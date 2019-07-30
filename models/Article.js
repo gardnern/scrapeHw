1d2060ce80ba63514eb65650298f5da4d1d18b98
gardnern <48269958+gardnern@users.noreply.github.com>
2019-07-30 13:03:32 -0600

    heroku linked

--- models/Article.js
+++ models/Article.js
@@ -7,33 +7,21 @@ var Schema = mongoose.Schema;
 // This is similar to a Sequelize model
 var ArticleSchema = new Schema({
   // `title` is required and of type String
-  headline: {
-    type: String,
-    required: true,
-    unique: true
-  },
-
-  author: {
+  title: {
     type: String,
     required: true
   },
   // `link` is required and of type String
-  summary: {
-    type: String,
-    required: true
-  },
-
-  url: {
+  link: {
     type: String,
     required: true
   },
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
   // This allows us to populate the Article with an associated Note
-  date: String,
-  saved: {
-    type: Boolean,
-    default: false
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
   }
 });
 
