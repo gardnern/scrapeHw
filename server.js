5be07cbf8c3a63506aeff0611869c0fb50cdc7f5
gardnern <48269958+gardnern@users.noreply.github.com>
2019-08-02 19:57:20 -0600

    heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -64,12 +64,12 @@ app.get("/scrape", function(req, res) {
             title: title,
             link: link
           },
-          function(error, saved) {
+          function(error, inserted) {
             if (error) {
               console.log(error);
             }
             else {
-              console.log(saved);
+              console.log(inserted);
             }
           });
       }
